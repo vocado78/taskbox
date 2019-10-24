e796edbaa63f62557b7d81112be03aa1bf271e77
vocado78 <hedeliningrid@gmail.com>
2019-10-24 13:39:33 -0700

    storybook get started guide

---
+++ .storybook/config.js
@@ -0,0 +1,15 @@
+import { configure } from '@storybook/react';
+import requireContext from 'require-context.macro';
+
+import '../src/index.css';
+
+const req = requireContext('../src/components', true, /\.stories\.js$/);
+
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+}
+
+configure(loadStories, module);
+
+// automatically import all files ending in *.stories.js
+// configure(require.context('../src/stories', true, /\.stories\.js$/), module);
